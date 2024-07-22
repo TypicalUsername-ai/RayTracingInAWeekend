09e020b55d6131ee79ce7e1b36eec0aa867f8a54
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-07-22 19:29:08 +0200

    Vec3: From [f32;3] impl

--- ray-tracer/src/vec3.rs
+++ ray-tracer/src/vec3.rs
@@ -19,3 +19,19 @@ mod default_test {
         assert_eq!(v.xyz, [0.0; 3])
     }
 }
+impl From<[f32; 3]> for Vec3 {
+    fn from(value: [f32; 3]) -> Self {
+        Self { xyz: value }
+    }
+}
+#[cfg(test)]
+mod from_tests {
+
+    use super::*;
+
+    #[test]
+    fn from_works() {
+        let v = Vec3::from([0.0, 0.1, 0.2]);
+        assert_eq!(v.xyz, [0.0, 0.1, 0.2])
+    }
+}
