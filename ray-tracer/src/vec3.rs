5f7350e56c0f3ee85bfa93a3731baacdedb39ca5
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-07-22 19:30:22 +0200

    Vec3: index<usize> impl

--- ray-tracer/src/vec3.rs
+++ ray-tracer/src/vec3.rs
@@ -35,3 +35,33 @@ mod from_tests {
         assert_eq!(v.xyz, [0.0, 0.1, 0.2])
     }
 }
+
+impl ops::Index<usize> for Vec3 {
+    type Output = f32;
+
+    /// this WILL panic!() if index > 2
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.xyz[index]
+    }
+}
+
+#[cfg(test)]
+mod index_tests {
+
+    use super::*;
+
+    #[test]
+    fn good_access() {
+        let v = Vec3::from([0.0, 0.2, 0.4]);
+        assert_eq!(v[0], 0.0);
+        assert_eq!(v[1], 0.2);
+        assert_eq!(v[2], 0.4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn bad_access() {
+        let v = Vec3::from([0.0, 0.2, 0.4]);
+        assert_eq!(v[4], 0.0);
+    }
+}
