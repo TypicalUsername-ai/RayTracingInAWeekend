7351597e535c625612894955a4eec0cf02b1783a
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-07-23 17:09:40 +0200

    chore: add docs and specify types for readability

--- ray-tracer/src/main.rs
+++ ray-tracer/src/main.rs
@@ -23,19 +23,28 @@ fn main() {
     let camera_center = Point::from([0.0; 3]);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    // veiwport_u x_min -> x_max
     let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    // viewport_v y_max -> y_min
     let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    let pixel_delta_u = viewport_u / width as f32;
-    let pixel_delta_v = viewport_v / height as f32;
+    // pixel_delta_u -> pixel step horizontal for the viewport
+    let pixel_delta_u: Vec3 = viewport_u / width as f32;
+    // pixel_delta_v -> pixel step vertical for the viewport
+    let pixel_delta_v: Vec3 = viewport_v / height as f32;
 
     // Calculate the location of the upper left pixel.
     let viewport_upper_left =
         camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+
+    // as the loop doesn't follow the xyz coordinates in the system we need to calculate the actual
+    // pixel 0 location
     let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
+    // stdout writer lock
     let mut lock = stdout().lock();
+    // stderr writer lock
     let mut err = stderr().lock();
     write!(lock, "P3\n{width} {height}\n255\n").unwrap();
     for y in 0..height {
