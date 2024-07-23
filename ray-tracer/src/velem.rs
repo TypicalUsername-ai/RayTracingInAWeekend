33862b9fa7d120538cc4cb48a1744e276cfcc4c4
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-07-23 15:13:30 +0200

    VElem: added the From<f32> trait maybe only temp

--- ray-tracer/src/velem.rs
+++ ray-tracer/src/velem.rs
@@ -1,5 +1,5 @@
 use num_traits::Float;
 
-pub trait VElem: Float + Copy + std::fmt::Display + Default {}
+pub trait VElem: Float + Copy + std::fmt::Display + Default + From<f32> {}
 
 impl VElem for f32 {}
