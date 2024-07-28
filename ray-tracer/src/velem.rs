98c95c356bfbdcd2037d649ec1b70e318b41c1d5
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-07-28 19:13:16 +0200

    velem: change trait to incorporate sampling possibility

--- ray-tracer/src/velem.rs
+++ ray-tracer/src/velem.rs
@@ -1,5 +1,14 @@
 use num_traits::Float;
 
-pub trait VElem: Float + Copy + std::fmt::Display + Default + From<f32> {}
+pub trait VElem:
+    Float
+    + From<f32>
+    + Default
+    + std::fmt::Display
+    + rand::distributions::uniform::SampleBorrow<Self>
+    + rand::distributions::uniform::SampleUniform
+{
+}
 
 impl VElem for f32 {}
+impl VElem for f64 {}
