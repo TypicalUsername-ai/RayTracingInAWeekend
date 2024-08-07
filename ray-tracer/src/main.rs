497c933e0b2741c883bc9c7200a1405d6253fb96
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-08-07 23:07:16 +0200

    main: made better working scene

--- ray-tracer/src/main.rs
+++ ray-tracer/src/main.rs
@@ -13,11 +13,35 @@ mod velem;
 
 type Point = vec3::Point3<f32>;
 type Camera = camera::Camera<f32>;
+type Color = color::Color<f32>;
 
 fn main() {
+    let ground = lambertian::Lambertian::new(Color::new(0.8, 0.8, 0.0));
+    let center = lambertian::Lambertian::new(Color::new(0.1, 0.2, 0.5));
+    let left = metal::Metal::new(Color::new(0.8, 0.8, 0.8));
+    let right = metal::Metal::new(Color::new(0.8, 0.6, 0.2));
+
     let world: hittable_list::HittableList<f32> = vec![
-        Rc::new(sphere::Sphere::new(Point::from([0.0, 0.0, -1.0]), 0.5)),
-        Rc::new(sphere::Sphere::new(Point::from([0.0, -100.5, -1.0]), 100.0)),
+        Rc::new(sphere::Sphere::new(
+            Point::from([0.0, 0.0, -1.2]),
+            0.5,
+            center,
+        )),
+        Rc::new(sphere::Sphere::new(
+            Point::from([-1.0, 0.0, -1.0]),
+            0.5,
+            left,
+        )),
+        Rc::new(sphere::Sphere::new(
+            Point::from([1.0, 0.0, -1.0]),
+            0.5,
+            right,
+        )),
+        Rc::new(sphere::Sphere::new(
+            Point::from([0.0, -100.5, -1.0]),
+            100.0,
+            ground,
+        )),
     ];
 
     // image
