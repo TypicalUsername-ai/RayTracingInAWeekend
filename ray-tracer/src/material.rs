b1f42bbd39a4adc91c06e58fd7df7da12b4800c2
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-08-07 22:07:21 +0200

    material: better function signature

--- ray-tracer/src/material.rs
+++ ray-tracer/src/material.rs
@@ -1,7 +1,8 @@
+use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
 use crate::velem::VElem;
 
 pub trait Material<T: VElem> {
-    fn scatter(&self, ray_in: Ray<T>) -> Option<HitRecord<T>>;
+    fn scatter(&self, ray_in: Ray<T>, hit: HitRecord<T>, attenuation: Color<T>) -> Ray<T>;
 }
