7679e77d663c02e15b6c0eb2f0045a045d468910
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-08-18 20:00:37 +0200

    spere: fixed the hit algo which fixes the dielectric issues

--- ray-tracer/src/sphere.rs
+++ ray-tracer/src/sphere.rs
@@ -27,33 +27,48 @@ impl<T: VElem> Hittable<T> for Sphere<T> {
         ray_t: std::ops::RangeInclusive<T>,
     ) -> Option<HitRecord<T>> {
         let o_to_c = self.center - ray.origin();
+
+        //quaratic equation coefficients
         let a = ray.direction().length_squared();
         let h = ray.direction().dot(&o_to_c);
         let c = o_to_c.length_squared() - self.radius * self.radius;
 
         let discriminant = h * h - a * c;
-        if discriminant < 0.0.into() {
+        if discriminant < T::zero() {
             None
         } else {
             let sqrtd = discriminant.sqrt();
 
             // Find the nearest root that lies in the acceptable range.
-            let root = (h - sqrtd) / a;
-            if !ray_t.contains(&root) {
-                let root = (h + sqrtd) / a;
-                if !ray_t.contains(&root) {
-                    return None;
-                }
-            }
-            let outward_normal = (ray.at(root) - self.center) / self.radius;
+            let root_1 = (h - sqrtd) / a;
+            let root_2 = (h + sqrtd) / a;
+
+            // Check if either root is in the acceptable range
+            let (t_near, t_far) = if ray_t.contains(&root_1) {
+                (root_1, root_2)
+            } else if ray_t.contains(&root_2) {
+                (root_2, root_2)
+            } else {
+                return None;
+            };
+
+            // Choose the closest root (t_near) that is valid and in range
+            let root = if t_near < t_far { t_near } else { t_far };
+
+            // Calculate the point of intersection and the normal at the hit point
+            let hit_point = ray.at(root);
+            let outward_normal = (hit_point - self.center) / self.radius;
+
+            // Create the hit record and set the face normal
             let mut hit_r = HitRecord {
                 t: root,
-                p: ray.at(root),
+                p: hit_point,
                 normal: outward_normal,
                 front_facing: false,
                 material: self.material.clone(),
             };
-            hit_r.set_face_normal(&ray, outward_normal);
+            hit_r.set_face_normal(ray, outward_normal);
+
             Some(hit_r)
         }
     }
