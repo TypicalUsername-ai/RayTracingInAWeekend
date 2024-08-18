97e9703bd816ab8d2a6c961a2e36aeaa73d56abf
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-08-18 19:42:25 +0200

    metal: account for no fuzz

--- ray-tracer/src/metal.rs
+++ ray-tracer/src/metal.rs
@@ -22,8 +22,11 @@ impl<T: VElem> Material<T> for Metal<T> {
         ray_in: &crate::ray::Ray<T>,
         hit: &crate::hittable::HitRecord<T>,
     ) -> Option<(crate::ray::Ray<T>, crate::color::Color<T>)> {
-        let reflected =
-            ray_in.direction().reflect(hit.normal) + Vec3::random_unit_vec() * self.fuzz;
+        let reflected = if self.fuzz == T::zero() {
+            ray_in.direction().reflect(hit.normal)
+        } else {
+            ray_in.direction().reflect(hit.normal) + Vec3::random_unit_vec() * self.fuzz
+        };
         let scattered = Ray::new(hit.p, reflected);
         if scattered.direction().dot(&hit.normal) > T::zero() {
             Some((scattered, self.albedo))
